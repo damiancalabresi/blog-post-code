52463a711ad3083f89a04c9a31947a2ffe9649a2
Damian Calabresi <damiancalabresi@gmail.com>
2015-12-04 18:50:41 -0300

    My first post

---
+++ 01-my-first-post/src/main/java/com/dcalabresi/my_first_post/Application.java
@@ -0,0 +1,12 @@
+package com.dcalabresi.my_first_post;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application {
+
+    public static void main( String[] args ) {
+        System.out.println("Hello world!");
+    }
+}
